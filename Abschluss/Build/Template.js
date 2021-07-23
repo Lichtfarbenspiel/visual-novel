b8faef019a1bc5829036a82118b7bbfd80387be3
Lichtfarbenspiel <s.belua.d@gmail.com>
2021-07-23 19:39:30 +0200

    Scenes enabled

--- Abschluss/Build/Template.js
+++ Abschluss/Build/Template.js
@@ -1139,10 +1139,10 @@ var Abschluss;
         gameMenu = Abschluss.ƒS.Menu.create(inGameMenu, menuOptions, "gameMenu");
         // gameMenu.close();
         let scenes = [
-            // { scene: RockyBeach, name: "01RockyBeach", id: "01", next: "02" },
-            // { scene: TurtleBeach, name: "02TurtleBeach", id: "02", next: "null"},
-            // { scene: TrashBeach, name: "03TrashBeach", id: "03", next: "null"},  
-            // { scene: WaterBeach, name: "04WaterBeach", id: "04", next: "null"},
+            { scene: Abschluss.RockyBeach, name: "01RockyBeach", id: "01", next: "02" },
+            { scene: Abschluss.TurtleBeach, name: "02TurtleBeach", id: "02", next: "null" },
+            { scene: Abschluss.TrashBeach, name: "03TrashBeach", id: "03", next: "null" },
+            { scene: Abschluss.WaterBeach, name: "04WaterBeach", id: "04", next: "null" },
             { scene: Abschluss.GPGP, name: "05GPGP", id: "05", next: "null" },
             { scene: Abschluss.endScene, name: "EndScene", id: "end", next: "null" }
         ];
